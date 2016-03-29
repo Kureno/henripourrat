becbf2646e2fce0e74dd07b336a9ba709125b063
bastien.mottier@gmail.com <bastien.mottier@gmail.com>
2016-03-29 13:55:59 +0200

    Nettoyage css

--- accueil.php
+++ accueil.php
@@ -3,18 +3,6 @@
 Template Name: Accueil
 */
 ?><?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package Henri_Pourrat
- */
 
 get_header(); ?>
 <?php query_posts('showposts=3'); if (have_posts()) : while (have_posts()) : the_post(); ?>
@@ -32,7 +20,6 @@ get_header(); ?>
 <?php else : ?>
 
     <h1>Not Found</h1>
-    <p>Silly monkey.</p>
 
 <?php endif; wp_reset_query(); ?>
 <?php
