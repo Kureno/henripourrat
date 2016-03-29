62f4eeb51a6cf27940cf7f5990b9d34686d51c0d
bastien.mottier@gmail.com <bastien.mottier@gmail.com>
2016-03-29 14:29:31 +0200

    section actualités

--- header.php
+++ header.php
@@ -14,7 +14,6 @@
             <header id="masthead" class="site-header" role="banner">
                 <nav id="site-navigation" class="main-navigation" role="navigation">
                     <img class='nav_logo' src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="logo sahp"/>
-                    <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'hp' ); ?></button>
                     <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
                 </nav><!-- #site-navigation -->
             </header><!-- #masthead -->
